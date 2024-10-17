eb709c5724ea9de7b5c8e03031d8f6a729f8b1ec
martinfrancois <f.martin@fastmail.com>
2024-10-17 02:30:48 +0200

    add useful annotations as a starting point

--- src/main/java/ch/fmartin/PerformanceBenchmark.java
+++ src/main/java/ch/fmartin/PerformanceBenchmark.java
@@ -1,7 +1,20 @@
 package ch.fmartin;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Warmup;
 
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 20, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class PerformanceBenchmark {
 
     @Benchmark
